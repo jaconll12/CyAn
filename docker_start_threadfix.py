f5d8d18608cfbf2acf2c11d1c9acfa4079bd7dfc
James C Lloyd <jamesclloyd@Jamess-MacBook-Pro.local>
2020-01-02 14:59:45 -0500

    Adding new threadfix docker image

--- docker_start_threadfix.py
+++ docker_start_threadfix.py
@@ -1,3 +1,3 @@
 import os
-
-os.system("docker start c2019a44cb1c7583f5a981d81754ce4bb51ea1e718b71341aed30d857fb0666a")
+#os.system("docker start c2019a44cb1c7583f5a981d81754ce4bb51ea1e718b71341aed30d857fb0666a")\
+os.system("docker run -d -p 8443:8443 jmbmxer/threadfix start")
